0ec80e719d60dda4c55f035ea98647e0f8248959
Furqan Abid <furqanabid@Furqans-MacBook-Air.local>
2025-11-12 14:42:41 -0500

    Fixed type error

--- src/app/api/analysis/[id]/route.ts
+++ src/app/api/analysis/[id]/route.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     await connectDB();
